4bb8db646497df7d905d285814337218a36818b6
Pedro Gabriel <pedrogabrielbhz@gmail.com>
2021-07-22 23:51:46 -0300

    implement distinguished enc. format (DER)

--- cryptoMath/signature.py
+++ cryptoMath/signature.py
@@ -10,22 +10,28 @@ class Signature:
     def __repr__(self) -> str:
         return "Signature({:x}, {:x})".format(self.r, self.s)
 
+    def der(self):
+        """distinguished encoding rules format"""
+        rbin = self.r.to_bytes(32, byteorder='big')
+        # remove all null bytes at the beginning
+        rbin = rbin.lstrip(b'\x00')
+        # if rbin has a high bit, add a \x00
+        if rbin[0] & 0x80:
+            rbin = b'\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin
+        sbin = self.s.to_bytes(32, byteorder="big")
+        # remove all null bytes at the beginning
+        sbin = sbin.lstrip(b'\x00')
+        # if sbin has a high bit, add a \x00
+        if sbin[0] & 0x80:
+            sbin = b'\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
+
 if __name__ == "__main__":
 
-    def hash256(s):
-        return sha256(sha256(s).digest()).digest()
-
-    e = int.from_bytes(hash256(b'a dirty secret'), 'big')
-    z = int.from_bytes(hash256(b'a nice message'), 'big')
-    k = 1234567890
-
-    r = (k*G).x.num
-    k_inv = pow(k, N-2, N)
-    s = (z+r*e) * k_inv % N
-    point = e*G
-
-    print('Point: ', point)
-    print('Z: ', hex(z))
-    print('R: ', hex(r))
-    print('S: ', hex(s))
-
+    s = Signature(
+        r=0x37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6,
+        s=0x8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec
+    )
+    print(s.der().hex())
